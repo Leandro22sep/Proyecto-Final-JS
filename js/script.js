e2812e412c588adb4804c30e1a2966320001b4bb
LeandroRomeroFuentes <leandroromerofuentes@gmail.com>
2022-05-28 10:12:17 -0300

    arreglo productos.json obtenido con fetch y empiezo a pegar sus objetos en el DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("HOLA MUNDO");
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+
+// se espera a que cargue el DOM para llamar a la función
+document.addEventListener("DOMContentLoaded",() => {
+    obtenerCarrito();
+})
+
+// funcion que usa fetch para obtener el objeto de productos.json
+const obtenerCarrito = async() => {
+    const responseCarrito = await fetch("productos.json");
+    const productos = await responseCarrito.json();
+    pintarCards(productos);
+}
+
+const pintarCards = productos => {
+    console.log(productos);
+}
