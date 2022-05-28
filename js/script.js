5c1dee3d66e122766fb82cf7cbfab26d1ce99a9f
LeandroRomeroFuentes <leandroromerofuentes@gmail.com>
2022-05-28 10:33:43 -0300

    cards generadas en el DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+//seccion del DOM donde voy a "pegar" los productos
 const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
 
 // se espera a que cargue el DOM para llamar a la función
 document.addEventListener("DOMContentLoaded",() => {
@@ -13,6 +15,16 @@ const obtenerCarrito = async() => {
     pintarCards(productos);
 }
 
+//"pego" los productos en el DOM. Titulo, precio e imagen del producto
 const pintarCards = productos => {
-    console.log(productos);
+    console.log(productos);//BORRARRRRRR
+    productos.forEach(producto => {
+        templateCard.querySelector("h5").textContent = producto.title;
+        templateCard.querySelector("p").textContent = producto.precio;
+        templateCard.querySelector("img").setAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector(".btn-dark").dataset.id = producto.id;
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    items.appendChild(fragment);
 }
